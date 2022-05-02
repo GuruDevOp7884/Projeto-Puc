86134a53890046e89050e307abf20342bb24c6d5
GuruDevOp <gugaalves92@hotmail.com>
2022-05-02 15:32:08 -0300

    adicionando carousel

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 function abrir_login(){
     window.open("login.html", "_self")
 }
+
+function abrir_cadastro(){
+    window.open("cadastro.html", "_self")
+}
+
+
+
+function fazerLogin(){
+    var usuario_digitado = document.getElementById("usuario").value
+    var test_usuario = /\d+/
+    testar = test_usuario.test(usuario_digitado)
+
+    if(testar == false){
+        alert('Seu usuario deve conter apenas números')
+    }
+
+    var senha_digitada = document.getElementById("senha").value
+
+    if(testar == true && usuario_digitado.length > 0 && senha_digitada.length > 0){
+        alert("logado com êxito")
+    }
+}
