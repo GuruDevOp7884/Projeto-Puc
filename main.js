a80ef47caecb0b88304bb59204b5604b4d71c377
GuruDevOp <gugaalves92@hotmail.com>
2022-05-05 21:39:07 -0300

    main

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
-function abrir_login(){
+function abrirLogin(){
     window.open("login.html", "_self")
 }
 
-function abrir_cadastro(){
+function abrirCadastro(){
     window.open("cadastro.html", "_self")
 }
 
+function logout(){
+    window.open("index.html")
+}
 
+function senhaRec(){
+    window.open("recuperacao.html", "_self")
+}
 
 function fazerLogin(){
     var usuario_digitado = document.getElementById("usuario").value
-    var test_usuario = /\d+/
-    testar = test_usuario.test(usuario_digitado)
+    var senha_digitada = document.getElementById("senha").value
+    var test_usuario = /\w+/
+    testar = test_usuario.test(usuario_digitado, senha_digitada)
+    
 
     if(testar == false){
-        alert('Seu usuario deve conter apenas números')
+        alert('Por favor digite seu usuário e senha')
     }
 
-    var senha_digitada = document.getElementById("senha").value
 
-    if(testar == true && usuario_digitado.length > 0 && senha_digitada.length > 0){
+    if(testar == true && usuario_digitado.length >= 5 && senha_digitada.length >=6 ){
         alert("logado com êxito")
+        
     }
+    
+    if(testar == true && usuario_digitado.length <= 4 ){
+        alert("Seu usuário deve conter pelomenos 5 caracteres")
+    }
+    
+    if(testar == true && usuario_digitado.length == 0 ){
+        alert("Por favor digite seu usuário")
+    }
+
+    if(testar == true && senha_digitada.length < 6 ){
+        alert("Sua senha deve conter pelomenos 6 caracteres")
+    }
+
 }
