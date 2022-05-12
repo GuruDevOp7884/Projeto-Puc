20aff63ddd3538a9b2be326ac152cbce5b7df782
GuruDevOp <gugaalves92@hotmail.com>
2022-05-12 17:06:24 -0300

    main

--- homepage.js
+++ homepage.js
@@ -1,17 +1,35 @@
 function adicionarItem() {
     produto_digitado = document.getElementById('nome_do_produto').value
     preco_digitado = document.getElementById('preco_do_produto').value
+    data_produto = document.getElementById('data')
+    hora_produto = document.getElementById('hora')
     if (produto_digitado.length > 0 && preco_digitado.length > 0){
         
     var tabela = document.getElementById('estoque')
     var nova_linha = tabela.insertRow(0)
     var nova_celula_prod = nova_linha.insertCell(0)
     var nova_celula_preco = nova_linha.insertCell(1)
+    var nova_celula_data = nova_linha.insertCell(2)
+    var nova_celula_hora = nova_linha.insertCell(3)
+    let data = new Date()
+    let data_produto = (adicionarZero(data.getDate())) + "/" + (adicionarZero(data.getMonth() + 1)) + "/" + (adicionarZero(data.getFullYear())); 
+    let hora = new Date()
+    let hora_produto = (adicionarZero(hora.getHours())) + ":" + (adicionarZero(hora.getMinutes())) + ":" + (adicionarZero(hora.getSeconds()));
     nova_celula_prod.innerHTML = produto_digitado
     nova_celula_preco.innerHTML = preco_digitado
+    nova_celula_data.innerHTML = data_produto
+    nova_celula_hora.innerHTML = hora_produto
     document.getElementById('nome_do_produto').value = ''
     document.getElementById('preco_do_produto').value = ''
     }
+    // função para adicionar o zero na data e hora
+    function adicionarZero(numero){
+      if(numero <= 9)
+         return "0" + numero;
+      else
+         return numero;   
+    }
+    
  
 }
 
