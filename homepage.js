0068f94ed9e46abd39c5f6ec93e744bed04ae120
GuruDevOp <gugaalves92@hotmail.com>
2022-06-03 16:58:57 -0300

    main

--- homepage.js
+++ homepage.js
@@ -41,6 +41,8 @@ function removerItem() {
 }
 
 
+
+
 $(function() {
   $(".exportToExcel").click(function(e){
     var table = $(this).prev('.table2excel');
@@ -58,8 +60,63 @@ $(function() {
       });
     }
   });
-  
 });
+
+
+
+//Emissão nota fiscal
+
+var tabela_nf = document.querySelector('table') //pega a tabela do estoque atualizada
+//a linha abaixo a transforma num array, que é uma coleção javascript pra armazenar informações
+var array_nf = [...tabela_nf.rows].map(x => [...x.querySelectorAll('td, th')].map(td => td.textContent))
+array_nf.shift() //apaga o título da tabela, pois não vamos precisar disso
+var tamanho_array = array_nf.length //verifica o tamanho do array 
+lista = [] //variável vazia pra armazenar números aleatórios
+for(x = 0; x < tamanho_array; x++){ //gera 1 número aleatório por ciclo. A qtd de ciclos é dada pelo tamanho do array
+  numero_aleatorio = [Math.floor(Math.random() *65536)] //gera números aleatórios pra nf
+  lista.push(numero_aleatorio) //coloca os números aleatórios na variável lista
+  lista = lista.flat() //transforma um nested array num array plano
+  }
+
+var tab = document.getElementById('tabela')
+var linhas = tab.getElementsByTagName('tr')
+
+for(var x = 0; x < linhas.length; x++) {
+   var linha = linhas[x]
+   linha.addEventListener('click', function(){
+     selecionar_linha(this, false)
+   });
+}
+
+function selecionar_linha(linha, multiplas) {
+  if(!multiplas) {
+    var linhas = linha.parentElement.getElementsByTagName('tr');
+    for(var x = 0; x < linhas.length; x++){
+      var linha_dois = linhas[x];
+      linha_dois.classList.remove('selecionado');
+    }
+  }
+  linha.classList.toggle('selecionado')
+}
+
+var gerar_nota = document.getElementById('emitir_nf');
+gerar_nota.addEventListener('click', function(){
+  var selecionados = tab.getElementsByClassName('selecionado');
+  if(selecionados.length < 1){
+    alert('Selecione 1 linha, pelo menos');
+    return false;
+  }
+
+  for(var x= 0; x < selecionados.length; x++) {
+    var selecionado = selecionados[x];
+    selecionado = selecionado.getElementsByTagName('td');
+    document.write('Nota fiscal ---' + '<br>' + selecionado[0].textContent + '<br>' +  'Valor: ' + selecionado[1].textContent)
+    window.print()
+    window.location.href='homepage.html'
+  }
+  });
+
+
 
 // ICONES
 feather.replace()
